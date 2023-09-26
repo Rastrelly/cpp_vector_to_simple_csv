5a7441c772fb7a4915e482afd055a8bb02a2799d
Rastrelly <rastrelly@gmail.com>
2023-09-26 13:46:59 +0300

    Добавьте файлы проекта.

---
+++ cpp_exerc_26_09_23_2/cpp_exerc_26_09_23_2.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <vector>
+#include <fstream>
+
+struct dataPoint { int x, y; };
+
+std::vector<dataPoint> pts = {};
+
+int func(int x)
+{
+	return 10 * x*x + 2 * x - 25;
+}
+
+int main()
+{
+	int xmin = 0, xmax = 0;
+
+	std::cout << "Input XMIN\n";
+	std::cin >> xmin;
+	std::cout << "Input XMAX\n";
+	std::cin >> xmax;
+
+	if (xmin > xmax)
+	{
+		int buf = xmax; 
+		xmax = xmin;
+		xmin = buf;
+	}
+
+	int xcur = xmin;
+
+	do
+	{
+		pts.push_back({xcur,func(xcur)});
+		xcur++;
+	} while (xcur < xmax);
+
+	std::ofstream dataWriter("dataOutput.csv");
+	for (int i = 0; i < pts.size(); i++)
+	{
+		dataWriter << pts[i].x << ";" << pts[i].y << "\n";
+	}
+	dataWriter.close();
+	system("pause");
+}
